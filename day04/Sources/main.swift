20d1f50e751415a00d182e60adf95a49162fccff
Adrian Perez de Castro <aperez@igalia.com>
2024-12-04 15:40:50 +0200

    Day 04, Part 2

--- day04/Sources/main.swift
+++ day04/Sources/main.swift
@@ -9,6 +9,10 @@ while let line = readLine(strippingNewline: true) {
     board.append(Array(line.utf8))
 }
 
+let rows = board.count
+let cols = board[0].count
+print("Board size: \(rows)x\(cols)")
+
 let directions = [
     ( 1,  0), // Right
     (-1,  0), // Left
@@ -20,15 +24,16 @@ let directions = [
     (-1, -1), // Left-Up
 ]
 
-let rows = board.count
-let cols = board[0].count
-print("Board size: \(rows)x\(cols)")
+let asciiX = UInt8(ascii: "X")
+let asciiM = UInt8(ascii: "M")
+let asciiA = UInt8(ascii: "A")
+let asciiS = UInt8(ascii: "S")
 
 let XMAS: [UInt8] = [
-    UInt8(ascii: "X"),
-    UInt8(ascii: "M"),
-    UInt8(ascii: "A"),
-    UInt8(ascii: "S"),
+    asciiX,
+    asciiM,
+    asciiA,
+    asciiS,
 ]
 
 @MainActor
@@ -49,16 +54,70 @@ func isXMAS(position: (Int, Int), step: (Int, Int)) -> Bool
     return true
 }
 
-var result = 0
+// S.M
+// .A.
+// S.M
+let SM_SM = [asciiS, asciiM, asciiS, asciiM]
+
+// M.S
+// .A.
+// M.S
+let MS_MS = [asciiM, asciiS, asciiM, asciiS]
+
+// S.S
+// .A.
+// M.M
+let SS_MM = [asciiS, asciiS, asciiM, asciiM]
+
+// M.M
+// .A.
+// S.S
+let MM_SS = [asciiM, asciiM, asciiS, asciiS]
+
+let CrossMASCombinations = [SM_SM, MS_MS, SS_MM, MM_SS]
+
+
+@MainActor
+func isCrossMAS(position: (Int, Int)) -> Bool
+{
+    let (col, row) = position
+
+    if col < 1 || row < 1 || col >= (cols - 1) || row >= (rows - 1) {
+        return false
+    }
+
+    // There must be an "A" always in the middle
+    if board[row][col] != asciiA {
+        return false
+    }
+
+    let thisCombo = [board[row-1][col-1], board[row-1][col+1],
+                     board[row+1][col-1], board[row+1][col+1]]
+
+    for combo in CrossMASCombinations {
+        if thisCombo == combo {
+            return true
+        }
+    }
+
+    return false
+}
+
+var numXMAS = 0
+var numCrossMAS = 0
 for row in 0...board.count-1 {
     assert(cols == board[row].count)
     for col in 0...cols-1 {
         for directionStep in directions {
             if isXMAS(position: (col, row), step: directionStep) {
-                result += 1
+                numXMAS += 1
             }
+        }
+        if isCrossMAS(position: (col, row)) {
+            numCrossMAS += 1
         }
     }
 }
 
-print("Result: \(result)")
+print("Part 1: \(numXMAS)")
+print("Part 2: \(numCrossMAS)")
