27106e8b87408d79340e5804e3e31457d8588397
Adrian Perez de Castro <aperez@igalia.com>
2024-12-08 01:24:46 +0200

    Day 07, Part 2

--- day07/day07.py
+++ day07/day07.py
@@ -3,19 +3,38 @@
 
 from sys import stdin
 
-def solve(val, eqn):
+def solve1(val, eqn):
     if len(eqn) == 1:
         return eqn[0] == val
-    else:
-        return solve(val / eqn[-1], eqn[:-1]) or solve(val - eqn[-1], eqn[:-1])
 
+    return solve1(val / eqn[-1], eqn[:-1]) or solve1(val - eqn[-1], eqn[:-1])
 
-result = 0
+from operator import add, mul
+
+def cat(a: int, b: int) -> int:
+    return int(f"{a:d}{b:d}")
+
+def solve2(val, eqn):
+    if len(eqn) == 1:
+        return eqn[0] == val
+
+    for op in (add, mul, cat):
+        t = op(eqn[0], eqn[1])
+        if solve2(val, (t,) + eqn[2:]):
+            return True
+
+    return False
+
+result1 = 0
+result2 = 0
 for line in stdin.readlines():
     testval, eqn = line.split(":", maxsplit=1) 
     testval = int(testval)
     eqn = tuple(map(int, eqn.split()))
-    if solve(testval, eqn):
-        result += testval
+    if solve1(testval, eqn):
+        result1 += testval
+    if solve2(testval, eqn):
+        result2 += testval
 
-print(result)
+print("Part 1:", result1)
+print("Part 2:", result2)
