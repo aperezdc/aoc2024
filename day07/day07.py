2d14e48de1b0efe8f5d151943328f41f5ea15134
Adrian Perez de Castro <aperez@igalia.com>
2024-12-08 01:27:45 +0200

    Day 07, factor out redundancy

--- day07/day07.py
+++ day07/day07.py
@@ -2,28 +2,24 @@
 # SPDX-License-Identifier: MIT
 
 from sys import stdin
-
-def solve1(val, eqn):
-    if len(eqn) == 1:
-        return eqn[0] == val
-
-    return solve1(val / eqn[-1], eqn[:-1]) or solve1(val - eqn[-1], eqn[:-1])
-
 from operator import add, mul
 
 def cat(a: int, b: int) -> int:
     return int(f"{a:d}{b:d}")
 
-def solve2(val, eqn):
+def solve(operators, val, eqn):
     if len(eqn) == 1:
         return eqn[0] == val
 
-    for op in (add, mul, cat):
+    for op in operators:
         t = op(eqn[0], eqn[1])
-        if solve2(val, (t,) + eqn[2:]):
+        if solve(operators, val, (t,) + eqn[2:]):
             return True
 
     return False
+
+OPERATORS_PART1 = (add, mul)
+OPERATORS_PART2 = (add, mul, cat)
 
 result1 = 0
 result2 = 0
@@ -31,9 +27,9 @@ for line in stdin.readlines():
     testval, eqn = line.split(":", maxsplit=1) 
     testval = int(testval)
     eqn = tuple(map(int, eqn.split()))
-    if solve1(testval, eqn):
+    if solve(OPERATORS_PART1, testval, eqn):
         result1 += testval
-    if solve2(testval, eqn):
+    if solve(OPERATORS_PART2, testval, eqn):
         result2 += testval
 
 print("Part 1:", result1)
